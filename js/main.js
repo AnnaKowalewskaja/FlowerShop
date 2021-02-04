eef21e8a57c2dc1b933e18feba603b63c0b480d4
Anna Kovalevskaya <lao7777@ya.ru>
2021-02-04 22:41:17 +0300

    feat: header,review
    39:50, day 2

---
+++ js/main.js
@@ -0,0 +1,30 @@
+const flowersSlider = new Swiper('.flowers-slider', {
+    // Slider parameters
+    loop: true,
+    slidesPerView: 6,
+
+
+    // Navigation 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+});
+
+
+const reviwesSlider = new Swiper('.reviwes-slider', {
+    // Slider parameters
+    loop: true,
+    slidesPerView: 1,
+
+
+    // Navigation 
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+
+});
